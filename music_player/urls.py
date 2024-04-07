d6d71a5934b18535836f5185ada5e0e0a60ff1dd
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-04-08 01:43:59 +0530

    user login

--- music_player/urls.py
+++ music_player/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('media_app.url')),
+    path('',include('media_app.urls')),
+    path('user/',include('user.urls'))
 ]
 
 
