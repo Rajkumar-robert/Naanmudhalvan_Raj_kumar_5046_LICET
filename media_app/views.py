5d8a3963848e1146c3c0acca065ef849a8de21d5
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-04-08 02:59:16 +0530

    ss and ppt

--- media_app/views.py
+++ media_app/views.py
@@ -3,4 +3,9 @@ from .models import Song
 
 def indexPage(request):
     songs = Song.objects.all()
-    return render(request,'media_app/index.html',{'songs':songs})
+    favSongs = Song.objects.filter(audio_link=1)
+    return render(request,'media_app/index.html',{'songs':songs,'fav':favSongs})
+
+def allsongs(request):
+    songs = Song.objects.all()
+    return render(request,'media_app/allsongs.html',{'songs':songs})
